3a5b4c6cc7d3b92e6359bd73231d27f9ead4c183
rafaw <rafa.willian91@gmail.com>
2023-03-28 05:55:12 -0300

    Equals e Hashcode part 1 em colecoes do Tipo Set

---
+++ src/equalsAndHashCodesPart01/Account.java
@@ -0,0 +1,44 @@
+package equalsAndHashCodesPart01;
+
+import java.util.Objects;
+
+public class Account {
+
+    private final String number;
+    private final double balance;
+
+    public Account(String number, double balance) {
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "number='" + number + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    //Equals e HashCode ensinam o java que ele deve comparar a igualdade dentro de cada elemento.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Double.compare(account.balance, balance) == 0 && Objects.equals(number, account.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, balance);
+    }
+}
---
+++ src/equalsAndHashCodesPart01/Setapp2.java
@@ -0,0 +1,27 @@
+package equalsAndHashCodesPart01;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Setapp2 {
+
+    public static void main(String[] args) {
+
+        Account acc1 = new Account("123", 3000);
+        Account acc2 = new Account("564", 5000);
+        Account acc3 = new Account("564", 5000);
+
+        //Equals e HashCode ensinam o java que ele deve comparar a igualdade dentro de cada elemento.
+        //Por isso  acc3 não é adicionado. Senao o Java usa equals de classe object que compara referencia de memoria
+        var accs = new HashSet<Account>();//Set.of assim como List.of cria um Conjunto (Set) imutável.
+        accs.add(acc1);
+        accs.add(acc2);
+        accs.add(acc3);
+                                      // Ou seja, uma vez declarado nao pode ser mudado.
+        for(Account c : accs){
+            System.out.println(c);
+        }
+
+    }
+}
